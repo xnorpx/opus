bb684a2b49b381471f6a4c091c0e72f090ecbe04
Marcus Asteborg <maastebo@microsoft.com>
2019-01-26 17:56:58 -0800

    More SSE fixes

--- opus_config.cmake
+++ opus_config.cmake
@@ -3,35 +3,6 @@ include(opus_functions.cmake)
 configure_file(config.h.in config.h @ONLY)
 add_definitions(-DHAVE_CONFIG_H)
 
-opus_detect_sse(HAVE_SSE)
-opus_detect_neon(HAVE_NEON)
-
-include(CMakeDependentOption)
-cmake_dependent_option(OPUS_PRESUME_SSE
-                       "Use SSE always (requires CPU with SSE)"
-                       ON
-                       "HAVE_SSE;OPUS_PRESUME_SSE"
-                       OFF)
-cmake_dependent_option(OPUS_MAY_HAVE_SSE
-                       "Use SSE if available"
-                       ON
-                       "HAVE_SSE;NOT OPUS_PRESUME_SSE"
-                       OFF)
-
-if(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64")
-  set(OPUS_PRESUME_NEON ON)
-endif()
-cmake_dependent_option(OPUS_PRESUME_NEON
-                       "Use NEON always (requires CPU with NEON Support)"
-                       ON
-                       "HAVE_NEON;OPUS_PRESUME_NEON"
-                       OFF)
-cmake_dependent_option(OPUS_MAY_HAVE_NEON
-                       "Use NEON if available"
-                       ON
-                       "HAVE_NEON;NOT OPUS_PRESUME_NEON"
-                       OFF)
-
 option(FIXED_POINT "Use fixed-point code (for devices with less powerful FPU)"
        OFF)
 option(USE_ALLOCA "Use alloca for stack arrays (on non-C99 compilers)" OFF)
@@ -58,4 +29,75 @@ include(CheckLibraryExists)
 check_library_exists(m floor "" HAVE_LIBM)
 if(HAVE_LIBM)
   list(APPEND CMAKE_REQUIRED_LIBRARIES m)
+endif()
+
+opus_supports_cpu_detection(RUNTIME_CPU_CAPABILITY_DETECTION)
+
+include(CMakeDependentOption)
+
+opus_detect_sse(COMPILER_SUPPORT_SIMD)
+if(COMPILER_SUPPORT_SIMD)
+  if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(OPUS_PRESUME_SSE1 ON)
+    set(OPUS_PRESUME_SSE2 ON)
+  else()
+    cmake_dependent_option(OPUS_PRESUME_SSE1
+                           "Always use SSE1 (requires CPU with SSE1)"
+                           ON
+                           "COMPILER_SUPPORT_SIMD;OPUS_PRESUME_SSE1"
+                           OFF)
+    cmake_dependent_option(OPUS_PRESUME_SSE2
+                           "Always use SSE2 (requires CPU with SSE2)"
+                           ON
+                           "COMPILER_SUPPORT_SIMD;OPUS_PRESUME_SSE2"
+                           OFF)
+  endif()
+  cmake_dependent_option(OPUS_MAY_HAVE_SSE1
+                         "Do runtime check and use SSE1 if available"
+                         ON
+                         "COMPILER_SUPPORT_SIMD;NOT OPUS_PRESUME_SSE1"
+                         OFF)
+  cmake_dependent_option(OPUS_MAY_HAVE_SSE2
+                         "Do runtime check and use SSE2 if available"
+                         ON
+                         "COMPILER_SUPPORT_SIMD;NOT OPUS_PRESUME_SSE2"
+                         OFF)
+  cmake_dependent_option(OPUS_PRESUME_SSE4_1
+                         "Always use SSE4.1 (requires CPU with SSE4.1)"
+                         ON
+                         "COMPILER_SUPPORT_SIMD;OPUS_PRESUME_SSE4_1"
+                         OFF)
+  cmake_dependent_option(OPUS_MAY_HAVE_SSE4_1
+                         "Do runtime check and use SSE4.1 if available"
+                         ON
+                         "COMPILER_SUPPORT_SIMD;NOT OPUS_PRESUME_SSE4_1"
+                         OFF)
+  cmake_dependent_option(OPUS_PRESUME_AVX
+                         "Always use AVX (requires CPU with AVX)"
+                         ON
+                         "COMPILER_SUPPORT_SIMD;OPUS_PRESUME_AVX"
+                         OFF)
+  cmake_dependent_option(OPUS_MAY_HAVE_AVX
+                         "Do runtime check and use AVX if available"
+                         ON
+                         "COMPILER_SUPPORT_SIMD;NOT OPUS_PRESUME_AVX"
+                         OFF)
+endif()
+
+opus_detect_neon(COMPILER_SUPPORT_NEON)
+if(COMPILER_SUPPORT_NEON)
+  if(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64")
+    set(OPUS_PRESUME_NEON ON)
+  else()
+    cmake_dependent_option(OPUS_PRESUME_NEON
+                           "Always use NEON (requires CPU with NEON Support)"
+                           ON
+                           "COMPILER_SUPPORT_NEON;OPUS_PRESUME_NEON"
+                           OFF)
+  endif()
+  cmake_dependent_option(OPUS_MAY_COMPILER_SUPPORT_NEON
+                         "Use NEON if available"
+                         ON
+                         "COMPILER_SUPPORT_NEON;NOT OPUS_PRESUME_NEON"
+                         OFF)
 endif()
