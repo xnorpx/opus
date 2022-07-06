093b8dab25d5dc0ac25d7e4a47d03c6353e6cf79
Marcus Asteborg <maastebo@microsoft.com>
2022-07-06 11:39:29 -0700

    .

--- cmake/OpusPackageVersion.cmake
+++ cmake/OpusPackageVersion.cmake
@@ -6,7 +6,28 @@ set(__opus_version INCLUDED)
 function(get_package_version PACKAGE_VERSION PROJECT_VERSION)
 
   find_package(Git)
-  if(GIT_FOUND AND EXISTS "${CMAKE_CURRENT_LIST_DIR}/.git")
+  if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/package_version"
+         AND NOT OPUS_PACKAGE_VERSION)
+    # Not a git repo, lets' try to parse it from package_version file if exists
+    file(STRINGS package_version OPUS_PACKAGE_VERSION
+         LIMIT_COUNT 1
+         REGEX "PACKAGE_VERSION=")
+    string(REPLACE "PACKAGE_VERSION="
+                   ""
+                   OPUS_PACKAGE_VERSION
+                   ${OPUS_PACKAGE_VERSION})
+    string(REPLACE "\""
+                   ""
+                   OPUS_PACKAGE_VERSION
+                   ${OPUS_PACKAGE_VERSION})
+    # In case we have a unknown dist here we just replace it with 0
+    string(REPLACE "unknown"
+                   "0.0"
+                   OPUS_PACKAGE_VERSION
+                   ${OPUS_PACKAGE_VERSION})
+      message(STATUS "Opus package version from package_version file: ${OPUS_PACKAGE_VERSION}")
+
+  elseif(GIT_FOUND AND EXISTS "${CMAKE_CURRENT_LIST_DIR}/.git")
     execute_process(COMMAND ${GIT_EXECUTABLE}
                     --git-dir=${CMAKE_CURRENT_LIST_DIR}/.git describe
                     --tags --match "v*" OUTPUT_VARIABLE OPUS_PACKAGE_VERSION)
@@ -28,26 +49,6 @@ function(get_package_version PACKAGE_VERSION PROJECT_VERSION)
       message(STATUS "Opus package version from git repo: ${OPUS_PACKAGE_VERSION}")
     endif()
 
-  elseif(EXISTS "${CMAKE_CURRENT_LIST_DIR}/package_version"
-         AND NOT OPUS_PACKAGE_VERSION)
-    # Not a git repo, lets' try to parse it from package_version file if exists
-    file(STRINGS package_version OPUS_PACKAGE_VERSION
-         LIMIT_COUNT 1
-         REGEX "PACKAGE_VERSION=")
-    string(REPLACE "PACKAGE_VERSION="
-                   ""
-                   OPUS_PACKAGE_VERSION
-                   ${OPUS_PACKAGE_VERSION})
-    string(REPLACE "\""
-                   ""
-                   OPUS_PACKAGE_VERSION
-                   ${OPUS_PACKAGE_VERSION})
-    # In case we have a unknown dist here we just replace it with 0
-    string(REPLACE "unknown"
-                   "0"
-                   OPUS_PACKAGE_VERSION
-                   ${OPUS_PACKAGE_VERSION})
-      message(STATUS "Opus package version from package_version file: ${OPUS_PACKAGE_VERSION}")
   endif()
 
   if(OPUS_PACKAGE_VERSION)
@@ -59,8 +60,8 @@ function(get_package_version PACKAGE_VERSION PROJECT_VERSION)
   else()
     # fail to parse version from git and package version
     message(WARNING "Could not get package version.")
-    set(OPUS_PACKAGE_VERSION 0)
-    set(OPUS_PROJECT_VERSION 0)
+    set(OPUS_PACKAGE_VERSION 0.0)
+    set(OPUS_PROJECT_VERSION 0.0)
   endif()
 
   message(STATUS "Opus project version: ${OPUS_PROJECT_VERSION}")
